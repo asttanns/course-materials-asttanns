e616aa4e11b6c9736fa1127c84aba86c0b0e321d
asttanns <annisaastuti10@gmail.com>
2026-04-08 15:47:30 -0500

    activity.py

--- in-class-activities/03_GPU/activity.py
+++ in-class-activities/03_GPU/activity.py
@@ -1,4 +1,4 @@
-import numpy as np
+import cupy as cp
 import time
 
 t0 = time.time()
@@ -6,14 +6,14 @@ t0 = time.time()
 N = 10 ** 6
 
 # each random walk has 100 random normal steps
-steps = np.random.normal(loc=0, scale=1, size=(N, 100))
+steps = cp.random.normal(loc=0, scale=1, size=(N, 100), dtype=cp.float32)
 
 # each walk starts at 100 and then diverges based on randomly generated steps
 steps[:, 0] = 0
-r_walks = 100 + np.cumsum(steps, axis=1)
+r_walks = 100 + cp.cumsum(steps, axis=1)
 
-average_finish = np.mean(r_walks[:, -1])
-std_finish = np.std(r_walks[:, -1])
+average_finish = cp.mean(r_walks[:, -1])
+std_finish = .std(r_walks[:, -1])
 print(f'Average Finish: {average_finish}, Standard Deviation: {std_finish}')
 t1 = time.time()
 print(f'Time (s): {t1-t0}')
