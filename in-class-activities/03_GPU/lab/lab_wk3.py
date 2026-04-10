f5631b35f3d29e4b5e62221b9b9962be8e85748c
asttanns <annisaastuti10@gmail.com>
2026-04-10 15:21:33 -0500

    add code and sbatch files

---
+++ in-class-activities/03_GPU/lab/lab_wk3.py
@@ -0,0 +1,27 @@
+import cupy as cp
+import numpy as np
+ 
+n_elements = 10 ** 7
+arr_host = np.random.normal(size=n_elements)
+
+# Compute the mean of the array `arr_host` on the GPU and name it `mean_dev`
+# YOUR CODE HERE
+mean_dev = cp.mean(cp.asarray(arr_host))
+
+# On the GPU: subtract the mean (computed above) from each element
+# and square the result, naming the resulting array `squared_deviations_dev`
+# YOUR CODE HERE
+squared_deviations_dev = cp.square(cp.asarray(arr_host) - mean_dev)
+
+# On the GPU: calculate the average sum of squares (i.e. the average of `squared_deviations_dev`)
+# and name it `variance_dev` (this is the population variance!)
+# YOUR CODE HERE
+variance_dev = cp.mean(squared_deviations_dev)
+
+# Send the mean and variance back to the CPU Host so that they can be printed below
+# use variable names `mean_host` and `variance_host`
+# YOUR CODE HERE
+mean_host = cp.asnumpy(mean_dev)
+variance_host = cp.asnumpy(variance_dev)
+
+print(f"Mean: {mean_host}, Variance: {variance_host}")
